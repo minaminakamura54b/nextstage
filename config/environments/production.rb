2a2ba5a6b0e3fae170552ea9a9f9478623a044e9
Minami Nakamura <nakamura0373@gmail.com>
2026-04-21 16:23:20 -0700

    HU

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
   # アップロードされたファイルをAWSに保存する
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
